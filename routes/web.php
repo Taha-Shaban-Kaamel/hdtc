9f09b6f2807d6a56c5fb6f799d318b5735349db6
mohamed fathy <mohamed@fathy.com>
2025-10-07 21:39:15 +0200

    subscription

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::middleware(['auth', 'verified', 'role:super admin'])->group(function () {
     })->name('dashboard');
 
     Route::get('courses',function(){
-
         return view('courses.index');
     })->name('courses');
 
