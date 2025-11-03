8d9c59cab0bceae70e103a4b9d878efd40a47741
Taha-Shaban-Kaamel <tahashaban743@gmail.com>
2025-11-03 02:36:37 +0200

    resolve check role middleware name

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'role' => \App\Http\Middleware\CheckRole::class,
+            'role' => \App\Http\Middleware\checkRole::class,
         ]);
 
         $middleware->appendToGroup('web', [webSetLocale::class]);
